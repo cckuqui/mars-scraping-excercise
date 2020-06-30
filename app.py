5c2151c969e105c42d6e08fa6b483ff441e2b872
Cris B <cristina.bardan@gmail.com>
2020-06-30 15:41:35 -0500

    clear dictionary for init

--- app.py
+++ app.py
@@ -11,7 +11,18 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_data")
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-    
+    if mars is  None :
+        mars = {
+        'ntitle':'',
+        'nbody':'',
+        'feat_img':'',
+        'img_det':'',
+        'weather':'',
+        'facts':'',
+        'h':'',
+        'date':''
+        }
+
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
